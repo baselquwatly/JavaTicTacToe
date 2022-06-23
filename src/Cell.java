57d01b583a63a3174744a0e5a493c60a78241177
BaselQuwatly <basel_quwatly@outlook.com>
2022-06-23 13:13:38 +0300

    First Commit

---
+++ src/Cell.java
@@ -0,0 +1,20 @@
+class Cell {
+
+    private String value;
+
+    Cell() {
+        this.value = " ";
+    }
+
+    String getValue() {
+        return this.value;
+    }
+
+    void setValue(String value) {
+        this.value = value;
+    }
+
+    boolean empty() {
+        return this.value.equals(" ");
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,31 @@
+class Player {
+
+    private String name;
+    private String symbol;
+
+    Player() {
+        this.name = null;
+        this.symbol = null;
+    }
+
+    Player setName(String name) {
+        this.name = name;
+
+        return this;
+    }
+
+    String getName() {
+        return this.name;
+    }
+
+    Player setSymbol(String symbol) {
+        this.symbol = symbol;
+
+        return this;
+    }
+
+    String getSymbol() {
+        return this.symbol;
+    }
+
+}
